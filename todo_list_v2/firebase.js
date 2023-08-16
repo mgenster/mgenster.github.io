f86ca445c6e366eab97dd4692d6ec31ecd8c3522
mgenster <mgenster@gmail.com>
2023-08-16 16:38:01 +0300

    Added all foundations and v1 code

---
+++ todo_list_v2/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, doc, setDoc, addDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxOIm7rswOnGn346ArhDWVXwmUwcAAyqY",
+    authDomain: "todo-list-backend-3d745.firebaseapp.com",
+    projectId: "todo-list-backend-3d745",
+    storageBucket: "todo-list-backend-3d745.appspot.com",
+    messagingSenderId: "424400323009",
+    appId: "1:424400323009:web:15da67f3e5b7fb2f5d6f40",
+    measurementId: "G-KLQJS36Z99",
+    databaseURL: "https://todo-list-backend-3d745-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ----------- Cloud Firestore ---------------------------------------------------
+
+
+// Initialize Firestore and get a reference to the service
+const db = getFirestore(app);
+
+// Example
+await addDoc(collection(db, "todo-items"), {
+    status: "Incomplete",
+    text: "Clean the kitchen"
+  });
